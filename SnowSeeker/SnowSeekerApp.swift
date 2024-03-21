d3163ac22f9fb71faa75b03281e9bb12b13e23e3
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-21 21:20:51 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ramill Ibragimov on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
