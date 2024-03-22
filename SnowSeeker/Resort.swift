db0c229aeb22edf5b2fc0ac002939ca9f96c2600
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-22 10:10:28 +0200

    Building a primary list of items

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,25 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Ramill Ibragimov on 3/22/24.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
