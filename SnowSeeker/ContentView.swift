98e44e6ef999c07a719c88508530ed305f229793
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-22 09:25:45 +0200

    Searching

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var searchText = ""
+    let allNames = ["Jonh", "Mark", "Helena", "Viky"]
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { name in
+                name.localizedStandardContains(searchText)
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            Text("Search for \(searchText)")
-                .searchable(text: $searchText, prompt: "Look for something")
-                .navigationTitle("Searching")
+            List(filteredNames, id: \.self) { name in
+                    Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
         }
     }
 }
